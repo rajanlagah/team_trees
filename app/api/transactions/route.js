5188f8771384d85ff806fabaf4f9ed76f4db4aa4
rajanlagah <rajanlagah@gmail.com>
2023-07-18 23:06:33 +0530

    Added revalidatePath

--- app/api/transactions/route.js
+++ app/api/transactions/route.js
@@ -1,3 +1,4 @@
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -6,6 +7,8 @@ export async function GET(request){
     const params = {
         limit: 10
       };
+      const path = request.nextUrl.searchParams.get('/path') || '/'
+      revalidatePath(path)
       const paymentIntents = await stripe.paymentIntents.list(params);
       const successfulPayments = paymentIntents.data.filter(
         (pi) => pi.status === 'succeeded'
